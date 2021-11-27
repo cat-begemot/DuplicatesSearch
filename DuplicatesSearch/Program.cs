5e0b247d288dd5215b7f77a86d56c6b0face6e70
Alex Holenko <oholenko@vector-software.com>
2021-11-27 21:56:47 +0200

    Remove redundant usings

--- DuplicatesSearch/Program.cs
+++ DuplicatesSearch/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Text;
 
 namespace DuplicatesSearch
 {
-	internal class Program
+    internal class Program
 	{
 		private static String originFullPath, originName;
 		private static String checkingFullPath, checkingName;
